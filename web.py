27c365f98907653395b0ac4f06e96b23faf2202f
aqilzdhr <76737529+aqilzdhr@users.noreply.github.com>
2022-12-29 12:59:51 +0800

    Update web.py

--- web.py
+++ web.py
@@ -1,19 +1,6 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
 
-# horizontal menu
-selected = option_menu(
-menu_title=none,
-  option=["Home", "Prediction", "Video" "ABout Us"],
-  icons=["house", "brain", "book", "man"],
-  menu_icon="cast",
-  default_index=0,
-  orientation="horizontal"
-)
 st.set_page_config(page_title="Flood Forecasting", page_icon="tada:")
 
 #header section
 st.title("Flood Forecasting")
-
-
-
