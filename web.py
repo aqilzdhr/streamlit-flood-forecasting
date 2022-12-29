061ade20a3c0230ae439caa0630a9df597a2feb5
aqilzdhr <76737529+aqilzdhr@users.noreply.github.com>
2022-12-29 13:14:24 +0800

    Update web.py

--- web.py
+++ web.py
@@ -3,4 +3,5 @@ import streamlit as st
 st.set_page_config(page_title="Flood Forecasting", page_icon="tada:")
 
 #header section
-st.title("Flood Forecasting")
+st.title("Homepage")
+st.sidebar.success("Select a page above")
